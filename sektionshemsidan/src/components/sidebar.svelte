4f75faab8c89d99ab483063441a1e502d320754a
gusno115 <gusno115@student.liu.se>
2022-10-05 18:49:00 +0200

    La till sidebar (punktlistan till vänster)

---
+++ sektionshemsidan/src/components/sidebar.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let active;
+</script>
+
+<html>  
+<ul>
+    <h1 id="p1">LOREM</h1>
+    <h1 id="p2">IPSUM</h1>
+    <h1 id="p3">DOLAR</h1>
+    <h1 id="p4"></h1>
+    <h1 id="p5"></h1>
+</ul>
+</html>
+
+<style>
+    h1 {
+      color: aliceblue;  
+    }
+</style>
