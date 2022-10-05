1a7ba3d4780bf3188f5389b250a6274f771b6f89
Alexander Eklöw <aleek874@student.liu.se>
2022-10-05 18:24:46 +0200

    senaste eskapaderna

---
+++ sektionshemsidan/src/components/navbar.svelte
@@ -0,0 +1,49 @@
+<script>
+	export let active;    
+</script>
+
+
+<nav> 
+    <img src="images/vector.png" alt="det är ju loggan hummer" />
+    <ul>
+        <li><a href="index.astro" class={active === "start" ? 'underlined' : ''}>Start</a></li>
+        <li><a href="" class={active === "student" ? 'underlined' : ''}>Student</a></li>
+        <li><a href="">Om sektionen</a></li>
+        <li><a href="">Förmedlemmar</a></li>
+        <li><a href="">Kontakt</a></li>
+    </ul>
+   
+</nav>
+
+
+<style>
+    li{
+
+    }
+    a{
+        text-decoration: none;
+        color: inherit;
+        
+
+    }
+
+    .underlined{
+        text-decoration: underline;
+    }
+
+    ul{
+        display: flex;
+        justify-content: space-between;
+        width: 400px;
+        list-style: none;
+        align-items: center;
+    }
+    
+    nav {
+        width: 100%;
+        background-color: green;
+        display: flex;
+        justify-content: space-between;
+        
+    }
+</style>
