b8fe09e5c9638c3a4bcdba40cd6baae30e7cea8b
gusno115 <gusno115@student.liu.se>
2022-11-02 19:54:05 +0100

    Fixa navbaren till figmadesignen (och grå bakgrund)

--- sektionshemsidan/src/components/navbar.svelte
+++ sektionshemsidan/src/components/navbar.svelte
@@ -4,18 +4,22 @@
 
 
 <nav> 
-    <img src="images/vector.png" alt="det är ju loggan hummer" />
+    <img id="logo" src="images/KogvetHuvet.svg" alt="det är ju loggan hummer" />
     <ul>
         <li><a href="/" class={active === "start" ? 'underlined' : ''}>Start</a></li>
         <li><a href="student" class={active === "student" ? 'underlined' : ''}>Student</a></li>
         <li><a href="sektionen">Om sektionen</a></li>
-        <li><a href="medlemmar">Förmedlemmar</a></li>
+        <li><a href="medlemmar">För medlemmar</a></li>
         <li><a href="kontakt">Kontakt</a></li>
     </ul>
    
 </nav>
 
 <style>
+    #logo {
+        padding: 15px;
+        padding-left: 60px;
+    }
     li{
         font-size: x-large;
         font: bold;
@@ -35,12 +39,12 @@
         width: 650px;
         list-style: none;
         align-items: center;
-        padding-right: 30px;
+        padding-right: 100px;
     }
     
     nav {
         width: 100%;
-        background-color: green;
+        background-color: rgba(3, 125, 79, 1);
         display: flex;
         justify-content: space-between;
         
