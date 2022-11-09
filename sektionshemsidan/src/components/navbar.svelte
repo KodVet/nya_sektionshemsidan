34bad7efb4e6966f9f6714e8fcb9b6874e66c498
gusno115 <gusno115@student.liu.se>
2022-11-09 19:15:16 +0100

    sticky funktion i javascript

--- sektionshemsidan/src/components/navbar.svelte
+++ sektionshemsidan/src/components/navbar.svelte
@@ -2,19 +2,38 @@
 	export let active;  
     let y;
     let nav_h;
+    let nav;
+
     let min_h = 75;
     let max_h = 107;
     let max_scroll = 100;
+    let sticky = false
     function nav_scroll() {
         if(0<=y && 100>=y){
             nav_h = String(max_h - ((max_h-min_h)/max_scroll)*y) + "px"
-            console.log(nav_h) 
+            console.log("nav_scroll", nav_h, y, nav.offsetTop) 
         }  
     }
+    
+    function sticky_nav(){
+        if (y >= nav.offsetTop){
+            sticky = true
+            console.log("I AM STICKY", nav.offsetTop)
+        }
+        else {
+            sticky=false
+        }
+}
+
+    function scroll_funcs() {
+        sticky_nav();
+        nav_scroll();
+    }
+    console.log("javascript funkar")
 </script>
 
-<svelte:window on:scroll={nav_scroll} bind:scrollY={y}/>
-<nav style="height:{nav_h}"> 
+<svelte:window on:scroll={scroll_funcs} bind:scrollY={y}/>
+<nav bind:this={nav} style="height:{nav_h}" class:stickied={sticky}> 
     <img id="logo" src="images/KogvetHuvet.svg" alt="det är ju loggan hummer" />
 <!--     <div>
         Du har scrollat {y} pixlar
@@ -54,13 +73,18 @@
         align-items: center;
         padding-right: 100px;
     }
-    
+
     nav {
-        position: fixed;
+        position: relative;
         width: 100%;
         background-color: rgba(3, 125, 79, 1);
         display: flex;
         justify-content: space-between;
         
+    }
+
+    .stickied{
+        position: fixed !important;
+        top: 0px;
     }
 </style>
