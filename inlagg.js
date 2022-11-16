5efd8ecd3d52d844bd7edd575441337c0b3d47ee
gusno115 <gusno115@student.liu.se>
2022-11-16 19:00:44 +0100

    början på funktionalitet för inlägg

---
+++ inlagg.js
@@ -0,0 +1,19 @@
+posts = [
+    {
+        title: "TITEL", 
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex"
+    },
+    {
+        title: "TITEL", 
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex"
+    },
+    {
+        title: "TITEL", 
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex"
+    },
+    {
+        title: "TITEL", 
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex"
+    },
+
+]
---
+++ src/components/list.svelte
@@ -0,0 +1,36 @@
+<script>
+
+export let title;
+export let post;
+
+posts = [
+    {
+        title: "TITEL", 
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex"
+    },
+    {
+        title: "TITEL", 
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex"
+    },
+    {
+        title: "TITEL", 
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex"
+    },
+    {
+        title: "TITEL", 
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex"
+    },
+
+
+
+</script>
+
+
+<ul>
+    <li> <title></title></li>
+    <li><p></p></li>
+    <li><p></p></li>
+    <li><p></p></li>
+    <li><p></p></li>
+    <li><p></p></li>
+</ul>
