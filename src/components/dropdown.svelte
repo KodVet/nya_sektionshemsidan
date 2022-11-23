be2c2291fa2d9cec6197d13511b072265e544a6a
gusno115 <gusno115@student.liu.se>
2022-11-23 19:28:30 +0100

    dropdown component (ska används i navbar)

---
+++ src/components/dropdown.svelte
@@ -0,0 +1,34 @@
+<script>
+    export let active
+</script>
+
+<div class="ddbutton">
+    <li><a href="/" class={active === "start" ? 'underlined' : ''}>Start</a></li>
+    <div class="ddcontent" style = "margin-top: 30px;">
+        <a href="#">Link 1</a>
+        <a href="#">Link 2</a>
+    </div>
+</div>
+
+<style>
+     .ddbutton:hover .ddcontent {
+      display:flex;
+    }
+    .ddcontent{
+        display: none;
+        position: fixed;
+        left: 0px;
+        background-color: #f9f9f9;
+        width:100%;
+        box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+        z-index: 1;
+    }
+
+    .ddcontent a {
+        color: black;
+        padding: 12px 16px;
+        text-decoration: none;
+        display: block;
+}
+
+</style>
