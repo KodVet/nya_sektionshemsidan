582750dd3cbf8e9be91ac68bc5696dde3cb32f54
Gustaf Norberg <gusno115@student.liu.se>
2022-12-07 01:10:13 +0100

    scroll plugin
    Utan denna så hanterar swup inte länkar till sektioner av en sida. Nu gör den det. Det är alltså relevant för sidebaren

--- public/swupApp.js
+++ public/swupApp.js
@@ -1,4 +1,7 @@
 import Swup from 'swup'
 import SwupHeadPlugin from '@swup/head-plugin'
+import SwupScrollPlugin from '@swup/scroll-plugin';
 
-const swup = new Swup();
+const swup = new Swup({
+  plugins: [new SwupHeadPlugin(), new SwupScrollPlugin(),]
+});
