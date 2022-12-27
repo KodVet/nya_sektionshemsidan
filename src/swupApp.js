eead147de547642758d44b657487017962678b14
gusno115 <gusno115@student.liu.se>
2022-12-27 23:42:08 +0100

    swup preload

--- src/swupApp.js
+++ src/swupApp.js
@@ -1,8 +1,9 @@
 import Swup from 'swup'
 import SwupHeadPlugin from '@swup/head-plugin'
+import SwupPreloadPlugin from '@swup/preload-plugin';
 import SwupScrollPlugin from '@swup/scroll-plugin';
 import SwupSlideTheme from '@swup/slide-theme';
 
 const swup = new Swup({
-  plugins: [new SwupHeadPlugin(), new SwupScrollPlugin(), new SwupSlideTheme()]
+  plugins: [new SwupHeadPlugin(), new SwupScrollPlugin(), new SwupSlideTheme(), new SwupPreloadPlugin]
 });
