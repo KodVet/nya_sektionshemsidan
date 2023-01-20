63854e3bee9c6ba9012b48257fade8647e92f5f9
Gustaf Norberg <gusno115@student.liu.se>
2023-01-20 20:43:14 +0100

    feat: swup ignorerar länkar till pdfs

--- src/swupApp.js
+++ src/swupApp.js
@@ -5,5 +5,8 @@ import SwupScrollPlugin from '@swup/scroll-plugin';
 import SwupSlideTheme from '@swup/slide-theme';
 
 const swup = new Swup({
-  plugins: [new SwupHeadPlugin(), new SwupScrollPlugin(), new SwupSlideTheme(), new SwupPreloadPlugin]
+  plugins: [new SwupHeadPlugin(), new SwupScrollPlugin(), new SwupSlideTheme(), new SwupPreloadPlugin()],
+  ignoreVisit: (href, { el } = {}) => (
+    el?.matches('[data-no-swup], [href$=".pdf"]')
+  )
 });
