84a007e924bb8a5990f2381ddfa718ad59cbc0ae
Gustaf Norberg <gusno115@student.liu.se>
2023-01-26 16:58:40 +0100

    feat: snyggare transitions
    Swup animerar inte längre skroll emellan sidor, utan endast på samma sida med annorlunda hash (alltså mellan inlägg). Deta ser nästan objekltivt bättre ut, och minskar rörelse, dock ser navbarens transition förjävlig ut nu.

--- src/swupApp.js
+++ src/swupApp.js
@@ -1,12 +1,25 @@
-import Swup from 'swup'
-import SwupHeadPlugin from '@swup/head-plugin'
-import SwupPreloadPlugin from '@swup/preload-plugin';
-import SwupScrollPlugin from '@swup/scroll-plugin';
-import SwupSlideTheme from '@swup/slide-theme';
+import Swup from "swup";
+import SwupHeadPlugin from "@swup/head-plugin";
+import SwupPreloadPlugin from "@swup/preload-plugin";
+import SwupScrollPlugin from "@swup/scroll-plugin";
+import SwupSlideTheme from "@swup/slide-theme";
+import SwupScriptsPlugin from "@swup/scripts-plugin";
+import SwupRouteNamePlugin from '@swup/route-name-plugin';
 
 const swup = new Swup({
-  plugins: [new SwupHeadPlugin(), new SwupScrollPlugin(), new SwupSlideTheme(), new SwupPreloadPlugin()],
-  ignoreVisit: (href, { el } = {}) => (
-    el?.matches('[data-no-swup], [href$=".pdf"]')
-  )
+  plugins: [
+    new SwupScriptsPlugin(),
+    new SwupHeadPlugin(),
+    new SwupScrollPlugin({
+      animateScroll: {
+        samePageWithHash: true,
+        betweenPages: false
+      },
+      doScrollingRightAway: false,
+    }),
+    new SwupSlideTheme({ reversed: true }),
+    new SwupPreloadPlugin(),
+  ],
+  ignoreVisit: (href, { el } = {}) =>
+    el?.matches('[data-no-swup], [href$=".pdf"]'),
 });
