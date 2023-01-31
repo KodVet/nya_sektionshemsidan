7c157d56add7d3700f940f23953e7a174af94367
Gustaf Norberg <gusno115@student.liu.se>
2023-02-01 00:13:16 +0100

    WIP

--- src/components/CMS/IframeLivePreview.tsx
+++ src/components/CMS/IframeLivePreview.tsx
@@ -1,65 +1,75 @@
 import type { TemplatePreviewProps } from "@staticcms/core";
 import { useEffect, useState, useRef, createElement } from "react";
-import { createRoot } from 'react-dom/client'
+import { createRoot } from "react-dom/client";
 import ReactMarkdown from "react-markdown";
-const IframeLivePreview = (
-  { entry, fields, widgetFor, widgetsFor, collection: { IframePreview: options }, document }
-) => {
-    const relevantFields = fields.filter(field => field.id)
-    // console.log("relevant fields: ", relevantFields)
-    let renderedElements = []
-    let elements = {}
-    const [container, setContainer] = useState()
-    function defineContent(iframe) {
-        const doc = document.getElementById('my-frame').contentDocument
-        setContainer(doc.getElementById(entry.data[options.id]))
-        relevantFields.map(field => {
-            const element = container?.getElementsByClassName(`${field.id}`)[0]
-            if (element && field.widget === 'markdown') {
-                renderedElements.push(createRoot(element))
-            }
-        })
-          console.log("renderedElements: ", renderedElements)
+const IframeLivePreview = ({
+  entry,
+  fields,
+  widgetFor,
+  widgetsFor,
+  collection: { IframePreview: options },
+  document,
+}) => {
+  const relevantFields = fields.filter((field) => field.id);
+  // console.log("relevant fields: ", relevantFields)
+  const elements = useRef([])
+  const [container, setContainer] = useState();
+
+  function defineContent(iframe) {
+    const doc = document.getElementById("my-frame").contentDocument;
+    setContainer(doc.getElementById(entry.data[options.id]));
+    relevantFields.map((field, index) => {
+      const element = container?.getElementsByClassName(`${field.id}`)[0];
+      if (element && field.widget === "markdown") {
+        
+        console.log(createElement(element))
+        elements.current[index] = createRoot(element)
+
+      }
+      else elements.current[index] = element
+      console.log(element)
+    });
   }
+
   function appendContent(iframe) {}
-  const firstUpdate = useRef(true);
+
   //här
   useEffect(() => {
-    relevantFields.map((field) => {
-        // if (firstUpdate.current) {
-        //     firstUpdate.current = false
-        //     return
-        // }
-        const element = container?.getElementsByClassName(`${field.id}`)[0]
-        if (!element) return
-        if (field.widget === 'markdown') {
-            
-            // console.log(renderedElements[0]?.render(widgetFor(entry.data[field.name])))
-            // element.innerHTML = render(reactMarkdown(entry.data[field.name]), element)
-        }
-        else element.innerHTML = entry.data[field.name]
-    })
-  })
+    // console.log("elements i useEffect: ", elements);
+    relevantFields.map((field, index) => {
+      const element = elements.current[index]
+      if (!element) return;
+      if (element._internalRoot) {
+          console.log("nu ska markdown uppdateras", element);
+        element.render(widgetFor(field.name));
+      } else {
+        console.log(" nu ska vi uppdaterta ett vanligt element", element)
+          element.innerHTML = entry.data[field.name];
+      } 
+    });
+  });
   return (
-    <div 
-    style={{
-        width: '100%',
-        height: '100%'
-    }}>
-        <button onClick={defineContent}></button>
-        <iframe style={{
-            width: '100%',
-            height: '100%'
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <button onClick={defineContent}>BEEG</button>
+      <iframe
+        style={{
+          width: "100%",
+          height: "100%",
         }}
         id="my-frame"
-          src={options.src + '#' + entry.data[options.id]}
-          onLoad={() => setTimeout(() => {
-              defineContent(this)
-
-          },4000)
-          }
-        ></iframe>
+        src={options.src + "#" + entry.data[options.id]}
+        onLoad={() =>
+          setTimeout(() => {
+            defineContent(this);
+          }, 500)
+        }
+      ></iframe>
     </div>
   );
 };
-export default IframeLivePreview
+export default IframeLivePreview;
