551e05b41ca64582430ac59cd7f54424552054b4
Gustaf Norberg <gusno115@student.liu.se>
2023-02-01 12:28:40 +0100

    wip

--- src/components/CMS/IframeLivePreview.tsx
+++ src/components/CMS/IframeLivePreview.tsx
@@ -1,6 +1,7 @@
 import type { TemplatePreviewProps } from "@staticcms/core";
 import { useEffect, useState, useRef, createElement } from "react";
 import { createRoot } from "react-dom/client";
+import {render } from 'react-dom'
 import ReactMarkdown from "react-markdown";
 const IframeLivePreview = ({
   entry,
@@ -10,43 +11,48 @@ const IframeLivePreview = ({
   collection: { IframePreview: options },
   document,
 }) => {
-  const relevantFields = fields.filter((field) => field.id);
-  // console.log("relevant fields: ", relevantFields)
+  const relevantFields = fields.filter((field) => field.class);
+  console.log("relevant fields: ", relevantFields)
   const elements = useRef([])
-  const [container, setContainer] = useState();
+//   const [container, setContainer] = useState();
 
-  function defineContent(iframe) {
+  function defineContent() {
     const doc = document.getElementById("my-frame").contentDocument;
-    setContainer(doc.getElementById(entry.data[options.id]));
+    // setContainer(doc.getElementById(entry.data[options.id]));
+    const container = doc.getElementById(entry.data[options.container])
+    console.log("doc", doc)
+    console.log("id inställmiomg", entry.data[options.container])
+    console.log("container", container)
     relevantFields.map((field, index) => {
-      const element = container?.getElementsByClassName(`${field.id}`)[0];
-      if (element && field.widget === "markdown") {
+      const element = container?.getElementsByClassName(`${field.class}`)[0];
+      if (element && field.widget === "markdown" || field.render === true) {
         
-        console.log(createElement(element))
         elements.current[index] = createRoot(element)
 
       }
       else elements.current[index] = element
+      console.log(field)
       console.log(element)
     });
   }
 
   function appendContent(iframe) {}
 
-  //här
-  useEffect(() => {
-    // console.log("elements i useEffect: ", elements);
-    relevantFields.map((field, index) => {
-      const element = elements.current[index]
-      if (!element) return;
-      if (element._internalRoot) {
-          console.log("nu ska markdown uppdateras", element);
-        element.render(widgetFor(field.name));
-      } else {
-        console.log(" nu ska vi uppdaterta ett vanligt element", element)
-          element.innerHTML = entry.data[field.name];
-      } 
-    });
+  relevantFields.map((field, index) => {
+      useEffect(() => {
+        const element = elements.current[index]
+        if (!element) return;
+        if (element._internalRoot) {
+            element.render(widgetFor(field.name));
+        } 
+        else if (element.getAttribute('src')) {
+            element.setAttribute('src', entry.data[field.name])
+        }
+        else {
+            console.log(" nu ska vi uppdaterta ett vanligt element", element)
+            element.innerHTML = entry.data[field.name];
+        } 
+    }, [entry.data[field.name]]);
   });
   return (
     <div
@@ -62,10 +68,10 @@ const IframeLivePreview = ({
           height: "100%",
         }}
         id="my-frame"
-        src={options.src + "#" + entry.data[options.id]}
+        src={options.baseSrc + entry.data[options.fieldSrc]?.toLowerCase() + "#" + entry.data[options.container]}
         onLoad={() =>
           setTimeout(() => {
-            defineContent(this);
+            defineContent();
           }, 500)
         }
       ></iframe>
