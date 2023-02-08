7620e26818623d4be9a250f4cb7127c027cb06ae
Victor Karlströmer <vicka592@student.liu.se>
2023-02-08 17:34:39 +0100

    commit victor

---
+++ src/components/victor_test.svelte
@@ -0,0 +1,19 @@
+<script>
+    let namn ='';
+    let lagom_namn ='';
+    export let efternamn;
+
+    function shorter() {
+        if (namn.length > 6){
+            lagom_namn = namn.slice(0, 6);
+            console.log(lagom_namn);
+        }
+        return lagom_namn
+    return namn
+    }
+</script>
+<input bind:value = {namn}/> 
+<p>
+    Tjenixen {shorter(namn)}, nice lagom namn du har!
+    Ditt efternamn e {efternamn}. elle hur?
+</p>
