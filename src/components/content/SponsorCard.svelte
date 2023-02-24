8c077ac99dd6184d55da6445ec9fcf6a08bab44e
Gustaf Norberg <gusno115@student.liu.se>
2023-02-24 14:37:37 +0100

    responsivitet wip

--- src/components/content/SponsorCard.svelte
+++ src/components/content/SponsorCard.svelte
@@ -3,32 +3,28 @@
 </script>
 
 <div id="SponsorCard">
-    
-        <h3>Sociala medier</h3>
-        <ul>
-            <li>
-                <!-- Länk till sektionens facebook sida -->
-                <a href="https://www.facebook.com/KogVet" target="_blank">
-                    <img class="Facebook" src= "https://mtb6000.dk/wp-content/uploads/2021/12/png-transparent-social-media-computer-icons-facebook-inc-icon-design-social-media.png">
-                </a>
-            </li>
-            <li>
-                <!-- Länk till sektionens instagram sida -->
-                <a href="https://www.instagram.com/kogvetstyret/" target="_blank">
-                    <img class="Instagram" src="https://www.freepnglogos.com/uploads/logo-ig-png/logo-ig-instagram-icon-instagram-logo-icon-instagram-logo-16.png">
-                </a>
-            </li>
-        </ul>
-
-        <hr class="solid">    
-
-        <h3>Våra Sponsorer</h3>
-        <ul>
+    <h3>KOGVET PÅ SOCIALA MEDIER</h3>
+    <ul class="socials">
+        <li>
+            <!-- Länk till sektionens facebook sida -->
+            <a href="https://www.facebook.com/KogVet" target="_blank" rel="noreferrer">
+                <img class="Facebook" src="https://mtb6000.dk/wp-content/uploads/2021/12/png-transparent-social-media-computer-icons-facebook-inc-icon-design-social-media.png" alt="Facebook logo">
+            </a>
+        </li>
+        <li>
+            <!-- Länk till sektionens instagram sida -->
+            <a href="https://www.instagram.com/kogvetstyret/" target="_blank" rel="noreferrer">
+                <img class="Instagram" src="https://www.freepnglogos.com/uploads/logo-ig-png/logo-ig-instagram-icon-instagram-logo-icon-instagram-logo-16.png" alt="Instagram logo">
+            </a>
+        </li>
+    </ul>
+    <h3>VÅRA SPONSORER!</h3>
+    <ul class="sponsors">
         <!-- Loopar igenom alla objekt i filen data/sponsorer/panel.json och skapar en länk av den ikonen som lagts till -->
         {#each sponsors as sponsor}
             <li>
                 <a href={sponsor.href}>
-                    <img class="spons" src={sponsor.img} alt="Sponsor Logga">
+                    <img class="sponsor-img" src={sponsor.img} alt="Sponsor Logga">
                 </a>
             </li>
         {/each}
@@ -36,37 +32,59 @@
 </div>
 
 
-<style>
+<style lang="scss">
 
     #SponsorCard{
         background-color: white;
-        width: 270px;
-        height: 300px;
-        margin: 0;
+        /* width: 270px; */
+        /* height: 300px; */
+        margin: auto;
         text-align: center;
-    }
-
-    .Facebook{
-        width : 60%;
-    }
-
-    .Instagram{
-        width : 60%;
+        h3 {
+            text-align: center;
+            color: black;
+        }
+        ul {
+            list-style: none;
+            padding: 0;
+            display:flex;
+            justify-content: space-around;
+            li {
+                flex-basis: 50%;
+                img {
+                    width : clamp(80px, 60%, 120px);
+                }
+            }
+        }
+        .socials {
+            img {
+            }
+        }
+        .sponsors {
+            img {
+            }
+        }
     }
 
     .solid{
-      border-top: 2px solid #bbb;  
+      border-top: 2px solid #bbb;
     }
 
-    ul {
-        list-style: none;
-        padding: 0;
-        display:flex;
-        justify-content: space-between;
+    @media (max-width: 900px) {
+        #SponsorCard {
+            ul {
+                flex-direction: column;
+                gap: 20px;
+            }
+        }
     }
-
-    .spons {
-        max-width: 100px;
+    @media (max-width: 800px) {
+        #SponsorCard {
+            ul {
+                flex-direction: row;
+                gap: 0px;
+            }
+        }
     }
 </style>
 
---
+++ src/components/content/TitleCard.svelte
@@ -0,0 +1,61 @@
+<script>
+    import SponsorCard from "./SponsorCard.svelte";
+    export let sponsors = false
+</script>
+
+<div class="container">
+    <div class="text" style="flex-basis: {sponsors ? '60' : '40'}%;">
+        <h1>
+            <slot name="header"/>
+        </h1>
+        <p>
+            <slot name="content" />
+        </p>
+    </div>
+    <div class="sponsors" style="background-color: {sponsors ? 'white' : 'lightgrey'}">
+        {#if sponsors}
+            <aside>
+                <SponsorCard/>
+            </aside>
+        {/if}
+    </div>
+</div>
+
+<style lang="scss">
+    .container {
+        position: relative;
+        border-radius: 3px;
+        width: 100%;
+        color: white;
+        display: flex;
+        justify-content: space-between;
+        .text {
+            flex-basis: 40%;
+            padding: 100px;
+            padding-top: 20px;
+            padding-bottom: calc(3em + 20px);
+            max-width: 110ch;
+            padding-left: calc(20vw - 130px);
+            background-color: var(--koggis-grön);
+        }
+        .sponsors {
+            flex-basis: 25%;
+            background-color: lightgray;
+        }
+    }
+
+    h1 {
+        font-size: 4rem;
+    }
+    p {
+        font-size: 1.5rem;
+    }
+    @media (max-width: 800px) {
+        .container {
+            flex-direction: column;
+            .text {
+                padding-inline: 30px;
+            }
+        }
+    }
+</style>
