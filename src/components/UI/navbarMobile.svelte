21da4697bd4c44c833a75a6bd9d0524c7d5959db
mzackrisson <80635449+mzackrisson@users.noreply.github.com>
2023-04-19 19:07:22 +0200

    stylade mobilnavbar, döpte om förra filen med mobil nav bar Co-authored-by: Skrubbadubba <Skrubbadubba@users.noreply.github.com>

---
+++ src/components/UI/navbarMobile.svelte
@@ -0,0 +1,68 @@
+<script> 
+import { pages } from '../../data/navbarConfig.json'
+
+
+
+</script>
+<div class="container">
+    <div class="mobileHeader"></div>
+    <nav>
+        <ul>
+            {#each pages as page}
+                <li>
+                    <details>
+                        <summary><a href={page.url}>{page.btnName}</a></summary>
+                        <ul class="dropdown">
+                            {#each page.childPages as childpage}
+                            {#if childpage}    
+                                <li><a href={childpage.url}>{childpage.btnName}</a></li>
+                            {/if}
+                            {/each}
+                        </ul>
+                    </details>
+                </li>
+            {/each}
+        </ul>
+    </nav>
+</div>
+
+<style lang="scss">
+
+    summary{
+        list-style: none;
+        text-decoration: none;
+        
+    }
+    summary::-webkit-details-marker {
+        display:none;
+    }
+    .container {
+        background-color: var(--koggis-grön);
+        .mobileHeader {
+            height: 50px;
+        }
+        ul{
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+            margin: 0px;
+            list-style: none;
+            text-decoration: none;
+            align-items:center;
+            padding: 0;
+            // margin: auto;
+            li{
+                width: fit-content;
+                a{
+                    text-decoration: none;
+                    color: black;
+                    &:hover {
+                        color: #FFFF;
+                        text-decoration: none;
+    
+                    }
+                }
+            }
+        }
+    }
+</style>
