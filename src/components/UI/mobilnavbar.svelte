4d0871cb7abed9b9745ecec646d30ed468a0aa50
gusno115 <gusno115@student.liu.se>
2023-09-10 02:07:26 +0200

    mötet innan sommarn

--- src/components/UI/mobilnavbar.svelte
+++ src/components/UI/mobilnavbar.svelte
@@ -1,17 +1,17 @@
 <script> 
-import { pages } from '../../data/navbarConfig.json'
+    import { pages } from '../../data/navbarConfig.json'
 
-
+    let expanded = false
 
 </script>
 <div class="container">
     <div class="mobileHeader">
-        <img src="/images/KogvetHuvet.svg" alt="det är ju loggan hummer" height="40px"/>
-        <button>dropdown</button>
+        <a href="/"><img src="/images/KogvetHuvet.svg" alt="det är ju loggan hummer" height="40px"/></a>
+        <button on:click={()=>expanded=!expanded}><img src="images/the-squirtle-squad.jpeg" height="40px"/></button>
     </div>
         
-    <hr>
-    <nav>
+    <nav class:expanded={expanded}>
+        <hr>
         <ul>
             {#each pages as page}
                 <li>
@@ -32,7 +32,6 @@ import { pages } from '../../data/navbarConfig.json'
 </div>
 
 <style lang="scss">
-
     summary{
         list-style: none;
         text-decoration: none;
@@ -42,10 +41,13 @@ import { pages } from '../../data/navbarConfig.json'
         display:none;
     }
     .container {
+        position: fixed;
+        z-index: 100;
         display: none;
-        background-color: var(--koggis-grön);
         .mobileHeader {
+            background-color: var(--koggis-grön);
             height: fit-content;
+            width: 100vw;
             display: flex;
             // flex-direction: column;
             justify-content: space-between;
@@ -56,26 +58,61 @@ import { pages } from '../../data/navbarConfig.json'
         hr {
             margin: 0;
         }
-        ul{ 
-            display: flex;
-            justify-content: center;
-            flex-direction: column;
-            margin: 0px;
-            list-style: none;
-            text-decoration: none;
-            align-items:center;
-            padding: 0;
-            // margin: auto;
-            li{
-                background-color: rgb(190, 244, 172);
-                width: fit-content;
-                a{
-                    text-decoration: none;
-                    color: black;
-                    &:hover {
-                        color: #FFFF;
+        button {
+                border: none;
+                cursor: pointer;
+                appearance: none;
+                background-color: var(--koggis-grön);
+        }
+        nav {
+            background-color: var(--koggis-grön);
+            clip-path: inset(100% 0 0 0);
+            transform: translateY(-100%);
+            transition: all .5s ease-in-out;
+            width: 100%;
+            height: calc(100vh - 85.33px);
+            &.expanded {
+                clip-path: inset(0% 0 0 0);
+                transform: translateY(0);
+            }
+            ul{ 
+                height: auto;
+                display: flex;
+                justify-content: center;
+                flex-direction: column;
+                margin: auto;
+                list-style: none;
+                text-decoration: none;
+                align-items:center;
+                padding: 0;
+                overflow: hidden;
+                transition: height .5s ease-in-out;
+                gap: 20px;
+                padding-top: 50px;
+                 
+
+                li{
+                    padding: 10px;
+                    background-color: var(--koggis-grön);
+                    
+                    
+                    //rgb(190, 244, 172);
+                    // Den här färgen på expanderade bakgrunder med svar text ^
+                    
+                    width: fit-content;
+                    a{
+                        font-size: 24pt;
                         text-decoration: none;
-    
+                        color: white;
+                        &:hover {
+                            color: #FFFF;
+                            text-decoration: none;
+                        }
+                        &:active { 
+                            text-decoration: dotted;
+                            text-decoration-color: red ;  
+                        }
+                        
                     }
                 }
             }
